34f9d45a648857b622804bc19adbbf3e2c8216bd
Paul G <paulsystem32@gmail.com>
2020-06-09 23:45:55 -0600

    Crear tarea nueva(HTML)
    -  Creamos la funcionalidad de
       añadir una nueva tarea.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import './styles.css'; // Incluimos los estilos globales
 import {ToDo,TodoList} from './classes';
+import { crearTodoHtml } from './js/componentes';
 
 const todoList = new TodoList();
 const tarea1 = new ToDo('Practicar javascript');
-const tarea2 = new ToDo('Comprar una silla gamer');
 todoList.nuevoTodo(tarea1);
-todoList.nuevoTodo(tarea2);
-// console.log({tarea});
-console.log({todoList});
+// console.log({tarea1});
+
+crearTodoHtml(tarea1);
--- src/js/componentes.js
+++ src/js/componentes.js
@@ -1,8 +1,20 @@
-import '../css/componentes.css';
+import { ToDo } from "../classes";
+// Referencias en el html
+const divTodoList = document.querySelector('.todo-list'); // Clase de css todo list.
 
-export const saludar = (nombre)=>{
-    console.log('Creando etiqueta h1 en el html');
-    const h1 = document.createElement('h1');
-    h1.innerText = `Hola ${nombre}`;
-    document.body.append(h1);
+export const crearTodoHtml = (todo) =>{
+    const htmlTodo = `
+    <li class="${(todo.completado) ? 'completed' : ''}" data-id="${todo.id}">
+        <div class="view">
+            <input class="toggle" type="checkbox" ${(todo.completado) ? 'checked' : ''}>
+            <label>${todo.tarea}</label>
+            <button class="destroy"></button>
+        </div>
+        <input class="edit" value="Create a TodoMVC template">
+    </li>`;
+    const div = document.createElement('div'); // creamos un div.
+    div.innerHTML = htmlTodo; // Al div creado le insertamos el bloque html.
+    divTodoList.append(div.firstElementChild); 
+    // a la clase de css todo-list le incrustamos el primer hijo del div creado. (li)
+    return div;
 }
